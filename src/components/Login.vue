7c624a7a9c20dcf33e01127734180d1f3931fa2f
shortnd <ococncol@gmail.com>
2018-09-16 00:03:34 -0400

    added Login methods

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -19,8 +19,40 @@
 </template>
 
 <script>
-export default {
+import axios from 'axios';
 
+export default {
+  data: () => ({
+    email: '',
+    password: '',
+    emailRules: [
+      v => !!v || 'E-mail is required',
+      v => /\S+@\S+\.\S+/.text(v) || 'E-mail must be valid',
+    ],
+  }),
+  methods: {
+    async submit() {
+      return axios.post('http://localhost:8081/users/login', {
+        email: this.email,
+        password: this.password,
+      }, { headers: {
+        'Content-Type': 'application/json',
+      } })
+        .then((response) => {
+          window.localStorage.setItem('auth', response.data.token);
+          this.$swal('Great', 'You are ready to start!', 'success');
+          this.$router.push({ name: 'Home' });
+        })
+        .catch((error) => {
+          const message = error.response.data.message;
+          this.$swal('Oh no!', `${message}`, 'error');
+          this.$router.push({ name: 'Login' });
+        });
+    },
+    clear() {
+      this.$refs.form.reset();
+    },
+  },
 };
 </script>
 
