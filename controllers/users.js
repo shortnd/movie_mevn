4a271c7fdcb09fbcf22acbeff27c3e6dee952ca6
shortnd <ococncol@gmail.com>
2018-09-16 23:28:50 -0400

    starting to make local stragity

--- controllers/users.js
+++ controllers/users.js
@@ -1,14 +1,40 @@
 const User = require('../models/User');
-
-const passportJWT = require('passport-jwt');
-const jwt = require('jsonwebtoken');
-
-const ExtractJwt = passportJWT.ExtractJwt;
-const jwtOptions = {};
-jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme('jwt');
-jwtOptions.secretOrKey = 'thisisthesecretkey';
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 
 module.exports.controller = (app) => {
+  // local strategy
+  passport.use(new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password',
+  }, (email, password, done) => {
+    User.getUserByEmail(email, (err, user) => {
+      if (err) { return done(err); }
+      if (!user) { return done(null, false); }
+      User.comparePassword(password, user.password, (error, isMatch) => {
+        if (isMatch) {
+          return done(null, user);
+        }
+        return done(null, false);
+      });
+      return true;
+    });
+  }));
+
+  // login a user
+  app.post('/users/login', passport.authenticate('local', { failureRedirect: '/users/login' }), (req, res) => {
+    res.redirect('/');
+  });
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser((id, done) => {
+    User.findById(id, (err, user) => {
+      done(err, user);
+    });
+  });
+
   // register a user
   app.post('/users/register', (req, res) => {
     const name = req.body.name;
@@ -27,29 +53,5 @@ module.exports.controller = (app) => {
       }
       res.send({ user });
     });
-  });
-
-  // login a user
-  app.post('/users/login', (req, res) => {
-    if (req.body.email && req.body.password) {
-      const email = req.body.email;
-      const password = req.body.password;
-      User.getUserByEmail(email, (err, user) => {
-        if (!user) {
-          res.status(404).json({ message: 'The user does not exist!' });
-        } else {
-          User.comparePassword(password, user.password, (error, isMatch) => {
-            if (error) throw error;
-            if (isMatch) {
-              const payload = { id: user.id };
-              const token = jwt.sign(payload, jwtOptions.secretOrKey);
-              res.json({ message: 'ok', token });
-            } else {
-              res.status(401).json({ message: 'The password is incorrect!' });
-            }
-          });
-        }
-      });
-    }
   });
 };
